a902157b43ccefcf2209d55ca964620ba4b88654
bergmannlucas <lucaasberg@gmail.com>
2019-01-15 17:22:54 -0200

    Add Procfile and update server port

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect('mongodb://goweek:goweek123@ds135760.mlab.com:35760/goweek-backend', {
   useNewUrlParser: true
 });
@@ -21,6 +23,6 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server started on port 3000');
 });
